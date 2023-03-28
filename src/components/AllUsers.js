3d33ece29aea7c9eb9dc32ca8c1610f02bdc7cf4
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2023-03-28 06:50:44 +0600

    Update AllUsers.js

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,76 +1,101 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Input } from 'reactstrap'
-import PaginationCom from './Pagination';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Input } from 'reactstrap';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import { data } from '../App';
 
 const AllUsers = () => {
-
   const allUsers = useContext(data);
 
-  const [userData, setUserData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [dataPerPage] = useState(8);
+const [currentPage, setCurrentPage] = useState(0);
+const [users, setUsers] = useState([]);
+const [searchInput, setSearchInput] = useState('');
+const [totalPages, setTotalPages] = useState(0);
 
-  useEffect(() => {
-    setUserData(allUsers);
-  }, [allUsers]);
+const perPage = 8;
+const pages = 10;
+const baseUrl = "https://api.github.com/users";
+
+useEffect(() => {
+  getUsers(currentPage);
+}, [currentPage]);
+
+// loading all users on first load
+const getUsers = async (page) => {
+  const token = "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";
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  
+  try {
+    const response = await fetch(`${baseUrl}?per_page=${perPage}&since=${page * perPage}`, { headers });
+    const data = await response.json();
+    setUsers(data);
+    setTotalPages(pages);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 
-
-  // this snippet is for pagination
-  let indextOfLastData = currentPage * dataPerPage;
-  let indexOfFirstData = indextOfLastData - dataPerPage;
-  let currentData = userData.slice(indexOfFirstData, indextOfLastData);
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-   // this function is for searching data from api
-  const searchResult = (search) => {
-    let searchData = allUsers.filter((data) => {
-      return (
-        data.login.toLowerCase().includes(search.toLowerCase()) ||
-        (data.name && data.name.toLowerCase().includes(search.toLowerCase()))
-      );
-    });
-    if (searchData.length > 0) {
-      setUserData(searchData);
+  // this function is for searching data from api
+  const handleSearch = (value) => {
+    if (value === "") {
+      getUsers(0);
+      setSearchInput("");
     } else {
-      setUserData(allUsers);
+      setSearchInput(value);
+      searchResult(value, 1);
+    }
+  };
+  
+  const searchResult = async (search, page) => {
+    const token = "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";
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  
+    try {
+      const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(search)}&per_page=${perPage}&since=${currentPage}`,{ headers });
+      const data = response.data;
+      setUsers(data.items);
+      setTotalPages(Math.ceil(pages));
+    } catch (error) {
+      console.error(error);
     }
   };
 
+ const handlePageClick = (data) => {
+  setCurrentPage(data.selected);
+};
 
 
 
+ 
   return (
     <>
       <div className='container'>
         <div className='search-container'>
-          <div className="row justify-content-center">
+          <div className='row justify-content-center'>
             <div className='col-12 mb-2'>
               <div className='search-box'>
-                <Input placeholder='Search By Username' onChange={(e) => searchResult(e.target.value)} />
+                <Input placeholder='Search By Username' value={searchInput} onChange={(e) => handleSearch(e.target.value)}/>
               </div>
             </div>
           </div>
         </div>
-        <div className="clearfix">
-          <div className="row">
-            {currentData.map(data => (
-              <div className="col-md-3 animated fadeIn mb-2" key={data.id}>
-                <Link to={`/user/${data.login}`} className="link-style">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="avatar">
-                        <img
-                          src={data.avatar_url}
-                          className="card-img-top"
-                          alt=""
-                        />
+        <div className='clearfix'>
+          <div className='row'>
+            {users?.map((data) => (
+              <div className='col-md-3 animated fadeIn mb-2' key={data.id}>
+                <Link to={`/user/${data.login}`} className='link-style'>
+                  <div className='card'>
+                    <div className='card-body'>
+                      <div className='avatar'>
+                        <img src={data.avatar_url} className='card-img-top' alt='' />
                       </div>
-                      <h5 className="card-title ">
-                        {data.login.toUpperCase()}
-                      </h5>
+                      <h5 className='card-title '>{data.login.toUpperCase()}</h5>
                     </div>
                   </div>
                 </Link>
@@ -80,13 +105,23 @@ const AllUsers = () => {
         </div>
         <div className='row justify-content-center'>
           <div className='col-4'>
-            <PaginationCom dataPerPage={dataPerPage} totalData={userData.length} paginate={paginate} />
+            <ReactPaginate
+              pageCount={totalPages}
+              onPageChange={handlePageClick}
+              containerClassName={'pagination'}
+              pageClassName={'page-item'}
+              pageLinkClassName={'page-link'}
+              previousClassName={'page-item'}
+              nextClassName={'page-item'}
+              previousLinkClassName={'page-link'}
+              nextLinkClassName={'page-link'}
+              activeClassName={'active'}
+            />
           </div>
         </div>
       </div>
     </>
-
-  )
-}
+  );
+};
 
 export default AllUsers
