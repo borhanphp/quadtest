75df02e6f38d6bc453f6db394b09f5ba0f5049b1
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2023-03-28 14:59:50 +0600

    added color mode

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import UserDetails from './components/UserDetails';
 import NavbarNav from './components/Navbar';
 
 const data = createContext();
+const ThemeContext = createContext(null);
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     getUserData();
@@ -23,20 +25,27 @@ function App() {
     setUsers(jsonData)
   };
 
+  // switching from one mode to another
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'dark' ? 'light' : 'dark'));
+  }
+
   return (
-    <data.Provider value={users}>
-      <div className="App">
-        <Router>
-          <NavbarNav />
-          <Routes>
-            <Route path='/user/:login' element={<UserDetails />} />
-            <Route path='/' element={<AllUsers />} />
-          </Routes>
-        </Router>
-      </div>
-    </data.Provider>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <data.Provider value={users}>
+        <div className="App" id={theme}>
+          <Router>
+            <NavbarNav />
+            <Routes>
+              <Route path='/user/:login' element={<UserDetails />} />
+              <Route path='/' element={<AllUsers />} />
+            </Routes>
+          </Router>
+        </div>
+      </data.Provider>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
-export { data };
+export { data, ThemeContext };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {
+  Button,
   Navbar,
   NavbarBrand,
 } from 'reactstrap';
+import {ThemeContext} from '../App';
 
 function NavbarNav(args) {
+  const toggle = useContext(ThemeContext);
+ 
 
   return (
     <div className='mb-2'>
-      <Navbar {...args} color='light'>
+      <Navbar {...args} color={toggle.theme === 'dark' ? 'dark' : 'light'}>
         <Link to='/' className='link-style'>
-          <NavbarBrand>UsersPoint</NavbarBrand>
+          <NavbarBrand  className={toggle.theme === 'dark' ? 'text-white' : 'text-secondary'}>UsersPoint</NavbarBrand>
         </Link>
+        <Button onClick={toggle.toggleTheme}>{toggle.theme === 'dark' ? 'Light Mode' : 'Dark Mode'}</Button>
       </Navbar>
     </div>
   );
